f9444317d875ce637a2c3ea7c7940bf3c98b6111
spacegirlsays <laurenvilela96@gmail.com>
2024-02-18 04:07:25 -0300

    first commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from pathlib import Path
+import pandas as pd
+import time
+
+browser_options = Options()
+file_path = str(Path(__file__).parent)
+browser_options.add_experimental_option("prefs", {
+  "download.default_directory": file_path
+  })
+
+print(file_path)
+#Iniciate browser
+browser = webdriver.Chrome(options=browser_options)
+browser.maximize_window()
+
+#Opening Site
+browser.get("https://rpachallenge.com/")
+
+#Download Excel File
+try:
+    file_exists = file_path.resolve(strict=True)
+except:
+    browser.find_element(By.XPATH, "//a[@class=' col s12 m12 l12 btn waves-effect waves-light uiColorPrimary center']").click()
+
+#Reading Excel
+excel_file = pd.read_excel("./challenge.xlsx", header=None, skiprows=1)
+for row in excel_file.itertuples():
+    #First Name
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelFirstName']").send_keys(row[1])
+    time.sleep(1)
+    #Last Name
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelLastName']").send_keys(row[2])
+    time.sleep(1)
+    #Company Name
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelCompanyName']").send_keys(row[3])
+    time.sleep(1)
+    #Role
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelRole']").send_keys(row[4])
+    time.sleep(1)
+    #Address
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelAddress']").send_keys(row[5])
+    time.sleep(1)
+    #Email
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelEmail']").send_keys(row[6])
+    time.sleep(1)
+    #Phone
+    browser.find_element(By.XPATH, "//input[@ng-reflect-name='labelPhone']").send_keys(row[7])
+    time.sleep(1)
+    browser.find_element(By.XPATH, "//input[@type='submit']").click()
+    time.sleep(1)
+
+browser.close()
